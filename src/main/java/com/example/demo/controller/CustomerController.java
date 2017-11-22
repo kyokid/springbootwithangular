ebe818b9520930bedc941901987ee3aad12a07b2
Ha Vo <havh0108@gmail.com>
2017-11-22 17:27:39 +0700

    first commit

---
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.entities.Customer;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.repositories.CustomerRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+//    @Autowired
+    CustomerRepository repository;
+
+    @RequestMapping(value = "/customer", method = RequestMethod.GET)
+    public List<Customer> getAll() {
+        List<Customer> list = new ArrayList<>();
+        Iterable<Customer> customers = repository.findAll();
+
+        customers.forEach(list::add);
+        return list;
+    }
+
+    @RequestMapping(value = "/findall")
+    public String findAll() {
+        String result = "";
+        for(Customer cust : repository.findAll()) {
+            result += cust.toString() + "<br>";
+        }
+        return result;
+    }
+
+    @PostMapping(value = "/postcustomer")
+    public Customer postCustomer(@RequestBody Customer customer) {
+        repository.save(new Customer(customer.getFirstName(), customer.getLastName()));
+        return customer;
+    }
+
+    @RequestMapping("/findbylastname")
+    public String fetchDataByLastName(@RequestParam("lastname") String lastName) {
+        String result = "";
+
+        for (Customer cust: repository.findByLastName(lastName)) {
+            result += cust.toString() + "<br>";
+        }
+        return result;
+    }
+
+
+}
